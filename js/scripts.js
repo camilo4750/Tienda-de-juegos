e1243e0654fa201a3f75487b2b87e4f24c68c43c
Camilo Andres Alarcon <camilo4750@gmail.com>
2021-11-10 19:30:46 -0500

    primer commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(e){
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclik($win, $navbar, width);
+
+    $win.resize(function(){
+        toggle_onclik($win, $navbar, width);
+    });
+
+    $toggle.click(function(e){
+        $navbar.toggleClass('toggle-left')
+    });
+});
+
+function toggle_onclik($win, $navbar, width){
+    if($win.width() <= 800){
+        $navbar.css({left: `-${width}px`})
+        document.getElementById('menu').style.display = 'block';
+    }else{
+        $navbar.css({left: '0px'});
+        document.getElementById('menu').style.display = 'none';
+    }
+}
